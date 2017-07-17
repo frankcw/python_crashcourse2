408666c0109ffd35846c853065a12d15a9526f60
frankcw <frankcordovaw@gmail.com>
2017-07-17 12:49:22 -0400

    not sure what got updated here

--- chapter4/buffet.py
+++ chapter4/buffet.py
@@ -1,14 +1,14 @@
-#buffet
+# buffet
 
 foods = ('beef','pork','fish','chinese','vegetables')
 
 for food in foods:
 	print(food.title())
 
-#python will reject this, because the list is a tuple.
-#food[0] = 'meat'
+# python will reject this, because the list is a tuple.
+# food[0] = 'meat'
 
-#the only way to modify a tuple is by replacing it.
+# the only way to modify a tuple is by replacing it.
 
 foods = ('eggs','soups','salads','vegan','drinks')
 print("\nThis is the modified Tuple:")
